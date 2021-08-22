e6c90c0b0aba77b0a45ae2eca2232eb07f709905
elvus <e.s95@hotmail.com>
2021-08-21 22:25:16 -0400

    Se agrego la ruta Adopta

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import "rbx/index.css";
-import { Navbar, Content, Section } from "rbx";
-import { FaGithub } from 'react-icons/fa';
-import InfoContainer from './componentes/pets';
+import { Navbar, Content } from "rbx";
+import Adopta from './componentes/pets';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
   return (
-    <Content>
-      <Navbar color="info">
-        <Navbar.Brand>
-          <Navbar.Item href="https://bulma.io">
-            <img
-              src={logo}
-              alt="Bulma: Free, open source, & modern CSS framework based on Flexbox"
-            />
-            <span> Busca Patitas</span>
-          </Navbar.Item>
-          <Navbar.Burger />
-        </Navbar.Brand>
-        <Navbar.Menu>
-          <Navbar.Segment align="start">
-            <Navbar.Item href="#" active={true}>Adopta</Navbar.Item>
-            <Navbar.Item href="#">Encuentra</Navbar.Item>
-            <Navbar.Item href="#">Ayuda</Navbar.Item>
-          </Navbar.Segment>
-        </Navbar.Menu>
-      </Navbar>
-      <Section>
-        <Content>
-          <InfoContainer/>
-        </Content>
-      </Section>
-    </Content>
+    <Router>
+      <Content>
+        <Navbar color="info">
+          <Navbar.Brand>
+            <Navbar.Item href="/">
+              <img
+                src={logo}
+                alt="Bulma: Free, open source, & modern CSS framework based on Flexbox"
+              />
+              <span> Busca Patitas</span>
+            </Navbar.Item>
+            <Navbar.Burger />
+          </Navbar.Brand>
+          <Navbar.Menu>
+            <Navbar.Segment align="start">
+              <Link className="navbar-item" to="/adopta">Adopta</Link>
+              <Navbar.Item href="#">Encuentra</Navbar.Item>
+              <Navbar.Item href="#">Ayuda</Navbar.Item>
+            </Navbar.Segment>
+          </Navbar.Menu>
+        </Navbar>
+      </Content>
+      <Switch>
+        <Route path="/adopta">
+          <Adopta />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
